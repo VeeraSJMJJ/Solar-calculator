9e5ca919f2bc0bf9d2db2cd0e522994d01772e63
VeeraSJMJJ <veeramanim7@gmail.com>
2025-07-07 20:41:25 +0530

    Add files via upload

--- App.py
+++ App.py
@@ -192,13 +192,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
-from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')  # Must be inside /templates folder
-
-if __name__ == '__main__':
-    app.run(debug=True)
